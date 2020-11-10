6c7b9a50f912f5b996ef58f3ac5b6e5a955ce5a0
H404j <hjlxy1314@139.com>
2020-11-10 14:45:35 +0800

    java数组的插入、删除

--- src/com/yu/suanfa/MyArray.java
+++ src/com/yu/suanfa/MyArray.java
@@ -1,39 +1,38 @@
 package com.yu.suanfa;
 
 public class MyArray {
-    private  int[] array;
-    private  int size;
+    private int[] array;
+    private int size;
 
     public MyArray(int cap) {
         this.array = new int[cap];
         size = 0;
     }
-    public void insert(int index,int element)throws Exception{
-        if (index<0||index>size){
+
+    public void insert(int index, int element) throws Exception {
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("超过数组实际元素范围");
         }
-        for (int i=size-1;i>=index;i--){
-            array[i+1]=array[i];
+        for (int i = size - 1; i >= index; i--) {
+            array[i + 1] = array[i];
         }
-        array[index]=element;
+        array[index] = element;
         size++;
     }
-    public  void output(){
-        for (int i = 0; i <size ; i++) {
+
+    public void output() {
+        for (int i = 0; i < size; i++) {
             System.out.println(array[i]);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        MyArray myArray=new MyArray(10);
-        myArray.insert(0,3);
+        MyArray myArray = new MyArray(10);
+        myArray.insert(0, 3);
+        myArray.insert(1, 7);
+        myArray.insert(2, 9);
+        myArray.output();
 
-       myArray.insert(1,7);
-       myArray.insert(2,9);
-
-
-      
- 
 
     }
 }
